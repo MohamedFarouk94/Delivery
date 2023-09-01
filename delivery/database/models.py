32fd23d1a12dbf58b0fcad6225357131e2496ab4
Mohamed Farouk <mohamedfarouk1994@gmail.com>
2023-09-01 22:48:36 +0300

    fixing database bug and creating sellers and customers

--- delivery/database/models.py
+++ delivery/database/models.py
@@ -13,13 +13,14 @@ class Person(models.Model):
 
 	def to_dict(self):
 		return {
+			'id': self.user.id,
 			'group': self.__class__.plural,
 			'first-name': self.user.first_name,
 			'last-name': self.user.last_name,
 			'username': self.user.username,
 			'status': self.status,
 			'email': self.user.email,
-			'date-joined': self.date_joined
+			'date-joined': self.user.date_joined
 		}
 
 
@@ -40,6 +41,9 @@ class Customer(Person):
 		return Order.objects.get(customer=self, status=pn)
 
 	def create_pending_order(self):
+		if len(Order.objects.filter(customer=self, status=pn)):
+			raise AttributeError('A pending order for this customer already exists.')
+
 		return Order.objects.create(customer=self)
 
 	def get_basket(self):
@@ -48,9 +52,8 @@ class Customer(Person):
 	def add_to_basket(self, item, quantity):
 		return self.get_pending_order().add_to_basket(item, quantity)
 
-	def make_order(self):
-		current_order = self.get_pending_order()
-		current_order.make_order()
+	def make_order(self, region):
+		self.get_pending_order().make_order(region)
 		self.create_pending_order()
 
 
@@ -61,14 +64,14 @@ class Pilot(Person):
 class Item(models.Model):
 	plural = 'items',
 
-	id = models.BigAutoField(primary_key=True),
-	name = models.CharField(max_length=50),
-	seller = models.ForeignKey(Seller, on_delete=models.CASCADE),
-	description = models.TextField(blank=True),
-	price = models.FloatField(),
-	rating = models.FloatField(null=True),
-	n_raters = models.IntegerField(default=0),
-	n_orders = models.IntegerField(default=0),
+	id = models.BigAutoField(primary_key=True)
+	name = models.CharField(max_length=50)
+	seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+	description = models.TextField(blank=True)
+	price = models.FloatField()
+	rating = models.FloatField(null=True)
+	n_raters = models.IntegerField(default=0)
+	n_orders = models.IntegerField(default=0)
 	n_buyouts = models.IntegerField(default=0)
 	# Later add image
 
@@ -90,40 +93,62 @@ class Item(models.Model):
 class Order(models.Model):
 	plural = 'orders',
 
-	id = models.BigAutoField(primary_key=True),
-	customer = models.ForeignKey(Customer, on_delete=models.CASCADE),
-	pilot = models.ForeignKey(Pilot, on_delete=models.SET_NULL, null=True),
-	status = models.CharField(max_length=10, choices=ORDER_STATUS_CHOICES, default=pn),
-	date_ordered = models.DateTimeField(null=None),
-	date_pilot_assigned = models.DateTimeField(null=None),
-	date_completed = models.DateTimeField(null=None)
+	id = models.BigAutoField(primary_key=True)
+	customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+	region = models.CharField(max_length=20, blank=True)
+	pilot = models.ForeignKey(Pilot, on_delete=models.SET_NULL, null=True)
+	status = models.CharField(max_length=10, choices=ORDER_STATUS_CHOICES, default=pn)
+	date_ordered = models.DateTimeField(null=True)
+	date_pilot_assigned = models.DateTimeField(null=True)
+	date_completed = models.DateTimeField(null=True)
+
+	def raise_error_if_not_pending(self):
+		if self.status != pn:
+			raise AttributeError('This order is already ordered.')
+
+	def raise_error_if_item_exists(self, item):
+		if len(BasketItem.objects.filter(order=self, item=item)):
+			raise AttributeError('This item is already in the basket.')
+
+	def raise_error_if_item_doesnt_exist(self, item):
+		if not len(BasketItem.objects.filter(order=self, item=item)):
+			raise AttributeError('This item is not in the basket.')
 
 	def get_basket(self):
 		return BasketItem.objects.filter(order=self)
 
 	def get_quantity_of_item(self, item):
-		return BasketItem.objects.get(order=self, item=item).get_quantity()
+		self.raise_error_if_not_pending()
+		try:
+			self.raise_error_if_item_doesnt_exist(item)
+			return BasketItem.objects.get(order=self, item=item).get_quantity()
+		except AttributeError:
+			return 0
 
 	def edit_quantity_of_item(self, item, quantity):
+		self.raise_error_if_not_pending()
+		self.raise_error_if_item_doesnt_exist(item)
+
 		BasketItem.objects.get(order=self, item=item).edit_quantity(quantity)
 
 	def add_to_basket(self, item, quantity):
-		if self.status != pn:
-			raise AttributeError('This order is already ordered.')
-
-		if len(BasketItem.objects.filter(order=self, item=item)):
-			raise AttributeError('This item is already in the basket.')
+		self.raise_error_if_not_pending()
+		self.raise_error_if_item_exists(item)
 
 		return BasketItem.objects.create(order=self, item=item, quantity=quantity)
 
 	def remove_from_basket(self, item):
+		self.raise_error_if_not_pending()
+		self.raise_error_if_item_doesnt_exist(item)
+
 		BasketItem.objects.filter(order=self, item=item).delete()
 
-	def make_order(self):
+	def make_order(self, region):
 		if self.status != pn:
 			raise AttributeError('This order is already ordered.')
 
 		self.status = od
+		self.region = region
 		self.date_ordered = dt.now()
 		self.save()
 
@@ -135,7 +160,8 @@ class Order(models.Model):
 			'pilot-id': self.pilot.user.id if self.pilot else 'N/A',
 			'pilot-username': self.pilot.user.username if self.pilot else 'N/A',
 			'status': self.status,
-			'date-purchased': self.date_purchased if self.date_purchased else 'N/A',
+			'date-ordered': self.date_ordered if self.date_ordered else 'N/A',
+			'date-pilot-assigned': self.date_pilot_assigned if self.date_pilot_assigned else 'N/A',
 			'date-completed': self.date_completed if self.date_completed else 'N/A'
 		}
 
@@ -144,8 +170,8 @@ class BasketItem(models.Model):
 	plural = 'basket_items',
 
 	id = models.BigAutoField(primary_key=True),
-	order = models.ForeignKey(Order, on_delete=models.CASCADE),
-	item = models.ForeignKey(Item, on_delete=models.CASCADE),
+	order = models.ForeignKey(Order, on_delete=models.CASCADE)
+	item = models.ForeignKey(Item, on_delete=models.CASCADE)
 	quantity = models.IntegerField(default=1)
 
 	def get_quantity(self):
--- delivery/database/testing_objects.py
+++ delivery/database/testing_objects.py
@@ -10,31 +10,37 @@ def create_user(User, first_name='', last_name='', email='', username='', passwo
 			password=password)
 
 
-def create_person(Person, **kwargs):
-	user = create_user(kwargs)
+def create_person(User, Person, **kwargs):
+	user = create_user(User, **kwargs)
 	return Person.objects.create(user=user)
 
 
-def create_all_customers(Customer):
-	customers_df = pd.read_csv('csvs/customers.csv')
+def create_all_customers(User, Customer):
+	customers_df = pd.read_csv('database/csvs/customers.csv')
 	for _, row in tqdm(customers_df.iterrows()):
 		kwargs = {'first_name': row.first_name,
 				'last_name': row.last_name,
 				'email': row.email,
-				'username': row.first_name.lowercase() + row.last_name.lowercase()}
-		customer = create_person(Customer, **kwargs)
+				'username': row.first_name.lower() + row.last_name.lower()}
+		customer = create_person(User, Customer, **kwargs)
 		customer.create_pending_order()
 
 
-def create_all_pilots(Pilot):
-	pilots_df = pd.read_csv('csvs/pilots.csv')
+def create_all_pilots(User, Pilot):
+	pilots_df = pd.read_csv('database/csvs/pilots.csv')
 	for _, row in tqdm(pilots_df.iterrows()):
 		kwargs = {'first_name': row.first_name,
 				'last_name': row.last_name,
 				'email': row.email,
-				'username': row.first_name.lowercase() + row.last_name.lowercase()}
-		create_person(Pilot, **kwargs)
+				'username': row.first_name.lower() + row.last_name.lower()}
+		create_person(User, Pilot, **kwargs)
 
 
-def create_all_sellers(Seller):
-	pass
+def create_all_sellers(User, Seller):
+	sellers_df = pd.read_csv('database/csvs/sellers.csv')
+	for _, row in tqdm(sellers_df.iterrows()):
+		kwargs = {'first_name': row.first_name,
+				'last_name': row.last_name,
+				'email': row.email,
+				'username': row.username}
+		create_person(User, Seller, **kwargs)
